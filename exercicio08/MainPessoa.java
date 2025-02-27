5f980644ff7ca7572fb6e4bb4ff9196debdf8d9b
Luís Felipe Silva Ferreira <152612333+IamLiper@users.noreply.github.com>
2025-02-27 23:05:13 +0000

    Salvando

--- exercicio08/MainPessoa.java
+++
@@ -1,9 +0,0 @@
-package exercicio08;
-
-public class MainPessoa {
-    public static void main(String[] args) {
-        Pessoa pessoa = new Pessoa("Lipe", Sexo.MASCULINO);
-
-        System.out.println(pessoa.toString());
-    }
-}
--- exercicio08/Sexo.java
+++
@@ -1,6 +0,0 @@
-package exercicio08;
-
-public enum Sexo {
-    MASCULINO,
-    FEMININO;
-}
