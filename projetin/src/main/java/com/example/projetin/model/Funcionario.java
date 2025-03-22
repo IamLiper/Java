9863a0862e26b1a4e0ea783411fed4afa7aa9495
Luís Felipe Silva Ferreira <152612333+IamLiper@users.noreply.github.com>
2025-03-21 21:42:26 -0300

    projetin vida

---
+++ projetin/src/main/java/com/example/projetin/model/Funcionario.java
@@ -0,0 +1,75 @@
+package com.example.projetin.model;
+
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Tab_Funcionario")
+public class Funcionario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    private String email;
+
+    private String matricula;
+
+    private String setor;
+
+    public Funcionario() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getSetor() {
+        return setor;
+    }
+
+    public void setSetor(String setor) {
+        this.setor = setor;
+    }
+
+    @Override
+    public String toString() {
+        return "Funcionario{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", matricula='" + matricula + '\'' +
+                ", setor='" + setor + '\'' +
+                '}';
+    }
+}
---
+++ projetin/src/main/java/com/example/projetin/repository/FuncionarioRepository.java
@@ -0,0 +1,10 @@
+package com.example.projetin.repository;
+
+import com.example.projetin.model.Funcionario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
+
+}
